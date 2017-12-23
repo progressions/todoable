5e2075b21c92fc3bee42365e888aff809089486b
Isaac Priestley <progressions@gmail.com>
2017-12-23 12:12:55 -0600

    Move public methods to the top and separate private methods

--- lib/todoable/todoable.rb
+++ lib/todoable/todoable.rb
@@ -5,53 +5,6 @@ class Todoable
     @token, @expires_at = get_token
   end
 
-  # curl \
-  # -u progressions@gmail.com:todoable \
-  # -H "Accept: application/json" \
-  # -H "Content-Type: application/json" \
-  # -X POST \
-  # http://todoable.teachable.tech/api/authenticate
-  #
-  def get_token
-    if expires_at.nil? || DateTime.now > expires_at
-      url = "http://todoable.teachable.tech/api/authenticate"
-      username = "progressions@gmail.com"
-      password = "todoable"
-      response = RestClient::Request.execute(method: :post, url: url, user: username, password: password, headers: {content_type: :json, accept: :json})
-      body = JSON.parse(response.body)
-
-      token = body["token"]
-      expires_at = DateTime.parse(body["expires_at"])
-
-      [token, expires_at]
-    else
-      [@token, @expires_at]
-    end
-  end
-
-  def request(method: :get, path:, params: {})
-    @token, @expires_at = get_token
-
-    uri = "http://todoable.teachable.tech/api/#{path}"
-    headers = {
-      "Authorization" => "Token token=\"#{@token}\"",
-      "Accept" => "application/json",
-      "Content-Type" => "application/json",
-    }
-
-    RestClient::Request.execute(method: method, url: uri, payload: params.to_json, headers: headers)
-  end
-
-  def get(path:, params: {})
-    response = request(method: :get, path: path, params: params)
-    JSON.parse(response.body)
-  end
-
-  def post(path:, params: {})
-    response = request(method: :post, path: path, params: params)
-    JSON.parse(response.body)
-  end
-
   def lists
     get(path: "lists")["lists"]
   end
@@ -103,4 +56,53 @@ class Todoable
     path = "lists/#{list_id}/items/#{item_id}"
     request(method: :delete, path: path)
   end
+
+  private
+
+    # curl \
+    # -u progressions@gmail.com:todoable \
+    # -H "Accept: application/json" \
+    # -H "Content-Type: application/json" \
+    # -X POST \
+    # http://todoable.teachable.tech/api/authenticate
+    #
+    def get_token
+      if expires_at.nil? || DateTime.now > expires_at
+        url = "http://todoable.teachable.tech/api/authenticate"
+        username = "progressions@gmail.com"
+        password = "todoable"
+        response = RestClient::Request.execute(method: :post, url: url, user: username, password: password, headers: {content_type: :json, accept: :json})
+        body = JSON.parse(response.body)
+
+        token = body["token"]
+        expires_at = DateTime.parse(body["expires_at"])
+
+        [token, expires_at]
+      else
+        [@token, @expires_at]
+      end
+    end
+
+    def request(method: :get, path:, params: {})
+      @token, @expires_at = get_token
+
+      uri = "http://todoable.teachable.tech/api/#{path}"
+      headers = {
+        "Authorization" => "Token token=\"#{@token}\"",
+        "Accept" => "application/json",
+        "Content-Type" => "application/json",
+      }
+
+      RestClient::Request.execute(method: method, url: uri, payload: params.to_json, headers: headers)
+    end
+
+    def get(path:, params: {})
+      response = request(method: :get, path: path, params: params)
+      JSON.parse(response.body)
+    end
+
+    def post(path:, params: {})
+      response = request(method: :post, path: path, params: params)
+      JSON.parse(response.body)
+    end
 end
